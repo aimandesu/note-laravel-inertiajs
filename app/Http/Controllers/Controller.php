304dcea405d4d3d62155e0c68c8b465c36a7fc9d
aimandesu <aimanafiqbinesam@gmail.com>
2025-04-16 18:35:50 +0800

    add response trait to use

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Traits\ApiResponse;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class Controller
 {
-    //
+    use ValidatesRequests, ApiResponse;
 }
